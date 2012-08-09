e3605623a2ff49bf7be3a89bb3db383c43243ac4
christina <christina.chang48@yahoo.com>
2012-08-09 14:19:16 -0500

    added icons and made things work

---
+++ Geaux Garden/VeggieTableCell.h
@@ -0,0 +1,13 @@
+//
+//  VeggieTableCell.h
+//  Geaux Garden
+//
+//  Created by iOS Camp on 8/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VeggieTableCell : UITableViewCell
+
+@end
